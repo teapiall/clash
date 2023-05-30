b16ab80a11a084c8e461f50de8a4b89beafff3c1
teapiall <shuaizheng06@126.com>
2023-05-30 11:33:23 +0800

    update

--- yaml.sh
+++ yaml.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-wget -O ./svc-clash/config.yaml "https://clashnode.com/wp-content/uploads/2023/05/20230522.yaml"
+wget -O ./svc-clash/config.yaml "https://clashnode.com/wp-content/uploads/2023/05/20230529.yaml"
