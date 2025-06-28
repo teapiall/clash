06f82c46435ea9b4484c83c94e758f48d9971977
teapiall <shuaizheng06@126.com>
2025-06-28 20:24:58 +0800

    update

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # clash
+# 网页打开 127.0.0.1:80
+
+# export -p //列出当前的环境变量值
+# export MYENV //定义环境变量
+# export MYENV=7 //定义环境变量并赋值
+
+
+# 利用 Export 命令使用代理
+
+export https_proxy=http://127.0.0.1:7890 http_proxy=http://127.0.0.1:7890 all_proxy=socks5://127.0.0.1:7890
+
+# 取消系统代理：
+
+unset  http_proxy  https_proxy  all_proxy
--- yaml.sh
+++ yaml.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 echo "wget -O 文件保存路径 下载文件地址url"
-wget -O ./svc-clash/config.yaml "https://clashnode.com/wp-content/uploads/2023/10/20231015.yaml"
+# wget -O ./svc-clash/config.yaml "https://clashnode.com/wp-content/uploads/2024/05/20240527.yaml"
+wget -O ./svc-clash/config.yaml "https://raw.githubusercontent.com/a2470982985/getNode/main/clash.yaml"
