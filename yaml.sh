c258024919967fb603b9235bf59ab92925289f9d
teapiall <shuaizheng06@126.com>
2025-06-28 21:33:34 +0800

    update

--- yaml.sh
+++ yaml.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 rm ./svc-clash/clash.yaml
+rm ./svc-clash/aclash.yaml
 echo "rm ./svc-clash/clash.yaml"
 
 echo "wget -O 文件保存路径 下载文件地址url"
 # wget -O ./svc-clash/clash.yaml "https://clashnode.com/wp-content/uploads/2024/05/20240527.yaml"
 wget -O ./svc-clash/clash.yaml "https://raw.githubusercontent.com/a2470982985/getNode/main/clash.yaml"
 echo "download OK"
-# sed -i '/\[SS\]/d' ./svc-clash/clash.yaml
+cp ./svc-clash/clash.yaml ./svc-clash/aclash.yaml
+sed -i '/\[SS\]/d' ./svc-clash/clash.yaml
 echo "del \[SS\] "
